fe7afa5cd3503e44251825630d0f18a324dfe3ee
Mykxo <ik_h291@outlook.com>
2025-05-13 08:51:50 -0400

    Début

--- depart_H25/src/Item/ItemBread.java
+++ depart_H25/src/Item/ItemBread.java
@@ -1,4 +1,12 @@
 package Item.Item;
 
 public class ItemBread extends Item{
+    protected String color;
+    protected double weight;
+
+    public ItemBread (int ID, String name, double price, String color, int weight){
+        super(ID,name,name,price);
+        this.color = color;
+        this.weight = weight;
+    }
 }
